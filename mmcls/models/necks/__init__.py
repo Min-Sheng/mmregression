3c42ed132fa050844e1dcd143c1455b5825b5b6e
Min-Sheng <mason840929@gmail.com>
2022-03-18 02:34:28 +0000

    Add AdaptiveAveragePooling

--- mmcls/models/necks/__init__.py
+++ mmcls/models/necks/__init__.py
@@ -1,6 +1,10 @@
 # Copyright (c) OpenMMLab. All rights reserved.
+from .aap import AdaptiveAveragePooling
 from .gap import GlobalAveragePooling
 from .gem import GeneralizedMeanPooling
 from .hr_fuse import HRFuseScales
 
-__all__ = ['GlobalAveragePooling', 'GeneralizedMeanPooling', 'HRFuseScales']
+__all__ = [
+        'GlobalAveragePooling', 'GeneralizedMeanPooling', 
+        'HRFuseScales','AdaptiveAveragePooling'
+    ]
